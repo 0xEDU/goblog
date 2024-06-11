c62da71287a3c82939d4a8b8e766b64fe56fd2d5
edu <bfwaend@gmail.com>
2024-06-11 00:54:13 -0300

    Consume from the API in UI

--- cmd/ui/main.go
+++ cmd/ui/main.go
@@ -1,7 +1,32 @@
 package main
 
-import "fmt"
+import (
+	"context"
+	"log"
+
+	"google.golang.org/grpc"
+	"google.golang.org/grpc/credentials/insecure"
+
+	pb "github.com/0xEDU/goblog/pkg/proto"
+)
+
+const GrpcPort = "9090"
 
 func main() {
-	fmt.Println("Hello, World!")
+	creds := grpc.WithTransportCredentials(insecure.NewCredentials())
+	conn, err := grpc.NewClient("localhost:"+GrpcPort, creds)
+	if err != nil {
+		log.Fatalf("failed to dial server: %v", err)
+	}
+	defer conn.Close()
+
+	client := pb.NewArticleServiceClient(conn)
+	response, err := client.GetArticleList(context.Background(), &pb.ArticleListRequest{})
+	if err != nil {
+		log.Fatalf("failed to get article list: %v", err)
+	}
+
+	for _, article := range response.Articles {
+		log.Printf("Article: %s", article.String())
+	}
 }
